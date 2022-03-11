93ff6d8bd8f7237a1c826e98511ae4f49804c64b
rickeysan <rickeysan95@gmail.com>
2022-03-11 16:28:44 +0900

    画面を表示した時に、画像をランダムで表示するようにした

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,35 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from '../axios'
+const jsonData = require('../all_picture.json')
+console.log('jsonDataです')
+// console.log(jsonData.objectIDs.length)
+console.log(
+  jsonData.objectIDs[Math.floor(Math.random() * jsonData.objectIDs.length)],
+)
+
+// type Picture = {
+//   title: string;
+//   primaryImage: string;
+// }
+
 export const Hero = () => {
+  const [picture, setPicture] = useState('')
+  useEffect(() => {
+    console.log('HeroのuseEffectです')
+    async function fetchData(fetchUrl: string) {
+      const request = await axios.get(fetchUrl)
+      console.log(request.data.primaryImage)
+      setPicture(request.data.primaryImage)
+      return request
+    }
+    const picture_id =
+      jsonData.objectIDs[Math.floor(Math.random() * jsonData.objectIDs.length)]
+    fetchData(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${picture_id}`,
+    )
+  }, [])
+
   return (
     <>
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -10,10 +41,7 @@ export const Hero = () => {
           </div>
         </div>
         <figure>
-          <img
-            src="https://api.lorem.space/image/album?w=800&h=600"
-            alt="Album"
-          />
+          <img src={picture} alt="Album" />
         </figure>
       </div>
     </>
