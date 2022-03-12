15253fd0fa9d07f247a6588c048f3e3ff25d146e
rickeysan <rickeysan95@gmail.com>
2022-03-12 13:25:44 +0900

    DBの設定を完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
+import { db } from './firebase/index'
 
 function App() {
   return (
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrrRI9iqWKdY9-dI8X19kxoOT8EPftnxM",
+    authDomain: "picture-app-9da4b.firebaseapp.com",
+    projectId: "picture-app-9da4b",
+    storageBucket: "picture-app-9da4b.appspot.com",
+    messagingSenderId: "742630780093",
+    appId: "1:742630780093:web:5356b7c8b6da5162141bd4",
+    measurementId: "G-FLRE5KFKJL"
+};
+
+export default firebaseConfig
