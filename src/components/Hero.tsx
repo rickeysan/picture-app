17c17aac0a4d202a95a77808bd6e14278eca5152
rickeysan <rickeysan95@gmail.com>
2022-03-12 09:47:20 +0900

    軽微な修正’ q

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
+import ReactLoading from 'react-loading'
 import axios from '../axios'
+import { PostCreate } from './PostCreate'
+
 const jsonData = require('../all_picture.json')
-console.log('jsonDataです')
-// console.log(jsonData.objectIDs.length)
-console.log(
-  jsonData.objectIDs[Math.floor(Math.random() * jsonData.objectIDs.length)],
-)
 
 // type Picture = {
 //   title: string;
@@ -15,33 +13,56 @@ console.log(
 
 export const Hero = () => {
   const [picture, setPicture] = useState('')
+
+  const makeRandomPictureId = () => {
+    return jsonData.objectIDs[
+      Math.floor(Math.random() * jsonData.objectIDs.length)
+    ]
+  }
+  const [pictureId, setPictureId] = useState(makeRandomPictureId())
+
+  const [loading, setLoading] = useState(false)
+
+  const changePicture = () => {
+    console.log('changePictureです')
+    setPictureId(makeRandomPictureId())
+  }
+
+  const fetchData = async (fetchUrl: string) => {
+    setLoading(true)
+    const request = await axios.get(fetchUrl)
+    console.log(request.data.primaryImage)
+    setPicture(request.data.primaryImage)
+    setLoading(false)
+    return request
+  }
+
   useEffect(() => {
     console.log('HeroのuseEffectです')
-    async function fetchData(fetchUrl: string) {
-      const request = await axios.get(fetchUrl)
-      console.log(request.data.primaryImage)
-      setPicture(request.data.primaryImage)
-      return request
-    }
-    const picture_id =
-      jsonData.objectIDs[Math.floor(Math.random() * jsonData.objectIDs.length)]
     fetchData(
-      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${picture_id}`,
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${pictureId}`,
     )
-  }, [])
+  }, [pictureId])
 
   return (
     <>
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+          <h2 className="card-title">絵画をじっくりと観察してみましょう</h2>
+          <PostCreate />
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Listen</button>
+            <button className="btn btn-primary" onClick={() => changePicture()}>
+              他の画像を見る
+            </button>
           </div>
         </div>
-        <figure>
-          <img src={picture} alt="Album" />
+        <figure className="w-6/12 h-96">
+          {loading && <ReactLoading type="spin" color="#696969" />}
+          <img
+            src={picture}
+            alt="Album"
+            className="w-full h-full object-contain"
+          />
         </figure>
       </div>
     </>
---
+++ src/components/PostCreate.jsx
@@ -0,0 +1,21 @@
+
+export const PostCreate = () => {
+    return (
+        <>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">ニックネーム</span>
+                </label>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+
+            </div>
+            <div class="form-control">
+                <label class="label">
+                    <span class="label-text">絵画を見て、気づいたことを書いてみましょう</span>
+                </label>
+                <textarea class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+            </div>
+            <button className="btn btn-primary">投稿する</button>
+        </>
+    )
+}
