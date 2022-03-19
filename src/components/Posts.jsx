e5baf29ac45c1127a99eb61cbfb3e2ebc89df458
rickeysan <rickeysan95@gmail.com>
2022-03-19 15:11:41 +0900

    部分一致で検索機能が完成

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,38 +5,63 @@ import { collection, getDoc, orderBy, query, onSnapshot, getDocs } from 'firebas
 import { Search } from "./Search"
 
 export const Posts = () => {
-    const [posts, setPosts] = useState([])
+    const [allPosts, setAllPosts] = useState([])
+    const [filteredPosts, setFilteredPosts] = useState([])
     const [searchText, setSearchText] = useState('')
+    const [isMounted, setIsMounted] = useState(false)
 
     useEffect(() => {
         const postData = collection(db, "posts")
         // console.log(postData)
         getDocs(postData).then((snapShot) => {
+            console.log('getDocsです')
             console.log(snapShot.docs.map((doc) => ({ ...doc.data() })))
-            setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })))
-            console.log('postsの中身')
-            console.log(posts)
+            setAllPosts(snapShot.docs.map((doc) => ({ ...doc.data() })))
+            setFilteredPosts(snapShot.docs.map((doc) => ({ ...doc.data() })))
+            // console.log('allPostsの中身')
+            // console.log(allPosts)
+            // setIsMounted(true)
         })
     }, [])
 
     useEffect(() => {
         console.log('searchTextが変更されました')
-        console.log(posts)
-        console.log(posts[0])
+        // console.log(allPosts)
+        // console.log(filteredPosts)
+        // console.log(posts)
+        // console.log(posts[0])
         // console.log(posts[0]['comment'])
         // postsの中身に対して、検索キーワードで絞り込みをかけて、新しい値をセットする
-        // const newPosts = posts.filter(filterPosts(searchText))
-        // console.log(newPosts)
-        // setPosts(newPosts)
+        // const newPosts = posts.filter(filterPosts)
+
+        if (searchText !== '') {
+            console.log('検索キーワードはあります')
+            const newPosts = allPosts.filter((sample) => {
+                // console.log(sample.comment)
+                const regexp = new RegExp(searchText)
+                console.log(regexp.test(sample.comment))
+                return regexp.test(sample.comment)
+            })
+            console.log('newPostsの中身')
+            console.log(newPosts)
+            setFilteredPosts(newPosts)
+        } else {
+            console.log('検索キーワードはありません')
+            console.log(allPosts)
+            setFilteredPosts(allPosts)
+        }
     }, [searchText])
 
     const handleSearchText = (value) => {
-        console.log('handleSearchTextメソッドです')
-        console.log(value)
+        // console.log('handleSearchTextメソッドです')
+        // console.log(value)
         setSearchText(value)
     }
 
     const filterPosts = (elm) => {
+        // console.log('filterPostsです')
+        // console.log(searchText)
+        // console.log(elm)
         const regexp = new RegExp('^' + searchText, 'i')
         return (elm.comment.match(regexp))
     }
@@ -45,7 +70,7 @@ export const Posts = () => {
         <div className="w-full">
             <h2>投稿一覧</h2>
             <Search handleSearchText={handleSearchText} />
-            {posts.map((post, index) => (
+            {filteredPosts.map((post, index) => (
                 <Post post={post} key={index} />
             ))}
         </div>
